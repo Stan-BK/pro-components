b45e176e9d9e3c44aaee005f603ce8faa90545f7
Stan-BK <2568143457@qq.com>
2025-03-19 16:01:16 +0800

    fix(isImg): 修复未包含检测图片资源是否为base64编码的问题

--- packages/pro-layout/src/utils/isImg/index.ts
+++ packages/pro-layout/src/utils/isImg/index.ts
@@ -1,8 +1,12 @@
 // source by https://github.com/ant-design/pro-components/blob/master/packages/utils/src/isImg/index.ts
 
+const imagePattern = 'png|jpg|jpeg|svg|webp|gif|bmp';
+const suffixPattern = new RegExp(`\\w.(${imagePattern})$`, 'i');
+const base64Pattern = new RegExp(`^data:image\\/(${imagePattern})`, 'i');
+
 /** 判断是否是图片链接 */
 function isImg(path: string): boolean {
-  return /\w.(png|jpg|jpeg|svg|webp|gif|bmp)$/i.test(path);
+  return suffixPattern.test(path) || base64Pattern.test(path);
 }
 
 export default isImg;
